6c21942ff286129692ec9f5a96aabb11b3dbf70a
Thiago Corrêa Amaro <thiagoamaro010399@gmail.com>
2023-01-13 10:52:28 -0300

    Move System

--- BurnOut/Assets/Scripts/PlayerRotation.cs
+++ BurnOut/Assets/Scripts/PlayerRotation.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerRotation : MonoBehaviour
 {
-    [SerializeField] private Vector3 _rotationSpeed;
+    [SerializeField] private float _rotationSpeed;
+    private Vector3 _rotation;
     private Rigidbody _rb;
 
     // Start is called before the first frame update
@@ -16,6 +18,13 @@ public class PlayerRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _rb.AddTorque(_rotationSpeed);
+        _rb.AddTorque(_rotation);
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 input = value.Get<Vector2>();
+        print(input);
+        _rotation = new Vector3(input.y, 0, -input.x) * _rotationSpeed;
     }
 }
